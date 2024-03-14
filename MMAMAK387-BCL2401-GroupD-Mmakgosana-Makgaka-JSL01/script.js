4f2b6b64ae2abc72e787825713db33755426b3e7
Mmakgosana <makgakammakgosana@gmail.com>
2024-03-14 09:00:37 +0200

    I have added the valid text with the green icon I have added an if statement in the js script

---
+++ MMAMAK387-BCL2401-GroupD-Mmakgosana-Makgaka-JSL01/script.js
@@ -0,0 +1,20 @@
+function validateSyntax() {
+    let input = document.getElementById('petInput').value;
+    // Validation logic goes here
+    let result=""; // Placeholder for validation result
+
+    // TODO: Write your validation logic here
+        // Check if input starts with 'pet_' and followed by alphanumeric characters
+        let pattern = /^pet_[0-9]{4}[aA-zZ]+/; //
+    // Check if the input matches the pattern
+        if(pattern.test(input)) {
+
+        result='Valid syntax <span class="circle"></span>'; // The code set result to valid if the pattern matches
+    }  else {
+        result = 'Invalid Syntax <span class=circle1"></span>'; // Set result to invalid if the pattern does not match 
+    }
+    // The code is to display the validation result
+    document.getElementById('result').innerHTML = result;
+}
+
+
